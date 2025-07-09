16aecddac53d2fb3e27214d461df4588da96a8f5
bunnystring <ziknaf_548@hotmail.com>
2025-07-08 19:57:53 -0500

    portfolio-ac-backend

---
+++ src/main/java/com/backend/portfolio_ac/PortfolioAcApplication.java
@@ -0,0 +1,13 @@
+package com.backend.portfolio_ac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioAcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioAcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/portfolio_ac/PortfolioAcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.portfolio_ac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioAcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
